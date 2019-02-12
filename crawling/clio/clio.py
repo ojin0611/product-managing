552bc4b90f6bbc9bbabde362cdb0f95a1a027239
Yang Youngjin <ojin0611@gmail.com>
2019-02-12 15:43:49 +0900

    how to import module with relative path

--- crawling/clio/clio.py
+++ crawling/clio/clio.py
@@ -5,101 +5,100 @@ import sys
 sys.path.append('../')
 from crawling import *
 
-
-def readNextPage():
-    html = driver.page_source
-    soup = bs(html, 'html.parser')
-    pages = soup.find('div',{'class':'paginate'}).find_all('a')
-    nextPage= False
-    for page in pages:
-        if nextPage:
-            driver.get(url_products + page['href'])
-            nextPage= False
-        if page.find('strong'):
-            nextPage= True
-
-def getItemList():
-    html = driver.page_source
-    soup = bs(html, 'html.parser')
-    items = soup.find('div',{'id':'pro_list'}).ul.find_all('li')
-    itemList=[]
-    for item in items:
-        itemList.append('http://www.cliocosmetic.com/ko/product/' + item.a['href'])
-
-    return itemList
-
-def seeDetailInfo(itemURL):
-    fp = urlopen(itemURL)
-    html = fp.read().decode("utf8")
-    fp.close()
-    soup = bs(html, 'html.parser')
-    seeDetail = soup.find('button',text='구매하러가기')['onclick']
-    return seeDetail[seeDetail.find('http://'):-1]
-    
-
-
-def getItem(itemURL):
-    fp = urlopen(itemURL)
-    html = fp.read().decode("utf8")
-    fp.close()
-    soup = bs(html, 'html.parser')
-
-    info = soup.find('div',{'class':'info_area'})
-
-    brandName = 'clio'
-
-    name = info.find('div',{'class':'tit'}).get_text().strip()
-    if brandName in name: name = name[len(brandName)+2:].strip()
-
-    image = soup.find('img',{'id':'overimage'})['src']
-
-    category = '#'
-
-    volume = soup.find('th',text='용량 또는 중량').parent.td.get_text().strip()
-    if not volume: volume = '#'
-
-    salePrice = getNumber(info.find('em',{'class':'point'}).get_text())
-
-    originalPrice = info.find('del')
-    originalPrice = getNumber(originalPrice.get_text()) if originalPrice else salePrice
-
-
-    item={'name':'#', 'url':'#', 'image':'#', 'salePrice':'#', 'originalPrice':'#', 'color':'#', 
-                       'category':'#', 'brand':'#','volume':'#'}
-    item['name'] = name
-    item['image']= image
-    item['category']=category
-    item['volume']=volume
-    item['salePrice']=salePrice
-    item['originalPrice']=originalPrice
-    item['brand']=brandName
-    item['url']=driver.current_url
-
-    items=[]
-    colorList = soup.find('select',{'id':'O_1'})
-    if colorList:
-        colors = colorList.find_all('option')
-        for color in colors:
-            color = color.get_text().strip()
-            if '옵션' in color:
-                continue
-            if '색상' in color:
-                continue
-            if '선택' in color:
-                continue
-
-            if 'NEW' in color:
-                color = color[4:].strip()
-                
-            item_copy = copy.deepcopy(item)
-            item_copy['color'] = color
-            items.append(item_copy)
-    else:
-        item['color']='#'
-        items.append(item)
-    return items
-
 def main():
+    def readNextPage():
+        html = driver.page_source
+        soup = bs(html, 'html.parser')
+        pages = soup.find('div',{'class':'paginate'}).find_all('a')
+        nextPage= False
+        for page in pages:
+            if nextPage:
+                driver.get(url_products + page['href'])
+                nextPage= False
+            if page.find('strong'):
+                nextPage= True
+
+    def getItemList():
+        html = driver.page_source
+        soup = bs(html, 'html.parser')
+        items = soup.find('div',{'id':'pro_list'}).ul.find_all('li')
+        itemList=[]
+        for item in items:
+            itemList.append('http://www.cliocosmetic.com/ko/product/' + item.a['href'])
+
+        return itemList
+
+    def seeDetailInfo(itemURL):
+        fp = urlopen(itemURL)
+        html = fp.read().decode("utf8")
+        fp.close()
+        soup = bs(html, 'html.parser')
+        seeDetail = soup.find('button',text='구매하러가기')['onclick']
+        return seeDetail[seeDetail.find('http://'):-1]
+        
+
+
+    def getItem(itemURL):
+        fp = urlopen(itemURL)
+        html = fp.read().decode("utf8")
+        fp.close()
+        soup = bs(html, 'html.parser')
+
+        info = soup.find('div',{'class':'info_area'})
+
+        brandName = 'clio'
+
+        name = info.find('div',{'class':'tit'}).get_text().strip()
+        if brandName in name: name = name[len(brandName)+2:].strip()
+
+        image = soup.find('img',{'id':'overimage'})['src']
+
+        category = '#'
+
+        volume = soup.find('th',text='용량 또는 중량').parent.td.get_text().strip()
+        if not volume: volume = '#'
+
+        salePrice = getNumber(info.find('em',{'class':'point'}).get_text())
+
+        originalPrice = info.find('del')
+        originalPrice = getNumber(originalPrice.get_text()) if originalPrice else salePrice
+
+
+        item={'name':'#', 'url':'#', 'image':'#', 'salePrice':'#', 'originalPrice':'#', 'color':'#', 
+                        'category':'#', 'brand':'#','volume':'#'}
+        item['name'] = name
+        item['image']= image
+        item['category']=category
+        item['volume']=volume
+        item['salePrice']=salePrice
+        item['originalPrice']=originalPrice
+        item['brand']=brandName
+        item['url']=driver.current_url
+
+        items=[]
+        colorList = soup.find('select',{'id':'O_1'})
+        if colorList:
+            colors = colorList.find_all('option')
+            for color in colors:
+                color = color.get_text().strip()
+                if '옵션' in color:
+                    continue
+                if '색상' in color:
+                    continue
+                if '선택' in color:
+                    continue
+
+                if 'NEW' in color:
+                    color = color[4:].strip()
+                    
+                item_copy = copy.deepcopy(item)
+                item_copy['color'] = color
+                items.append(item_copy)
+        else:
+            item['color']='#'
+            items.append(item)
+        return items
+
     path = 'chromedriver.exe' if (platform.system() == 'Windows') else '/Users/jg/Desktop/develop/DataTeam/DataProcessing/product/crawling/chromedriver'
     driver = webdriver.Chrome(path)
 
@@ -113,16 +112,20 @@ def main():
         try:
             itemList += getItemList()
             readNextPage()
-        except:
+        except Exception as e:
             break
 
     itemList = list(set(itemList))
     print('상품 개수 :', len(itemList))
     result=[]
+    start_time = time.time() 
+    #-------------------------------------------------------------#
     for i, itemURL in enumerate(itemList):
         print(i)
         itemURL = seeDetailInfo(itemURL)
         result += getItem(itemURL)
+    #-------------------------------------------------------------#
+    print("--- %0.2f seconds ---" %(time.time() - start_time))
 
     driver.close()
     output = json.dumps(result,ensure_ascii=False, indent='\t')
--- crawling/main.py
+++ crawling/main.py
@@ -5,22 +5,23 @@ def main():
     with open('brandlist.json', encoding='UTF-8') as brandlist:
         brand_dict = json.load(brandlist)
 
+    brand = sys.argv[1]
     for key, value in brand_dict.items():
-        if sys.argv[1] in value:
+        if brand in value:
             print('crawling method :',key)
             if key=='html':
-                pass
+                print(sys.path)
+                break
+
             if key=='api':
                 pass
             if key=='manual':
                 break
 
             break
-    
 
     try:
-        print('direcetory :',sys.argv[1])
-
+        print('directory :',sys.argv[1])
     except Exception as e:
         print("Error :", e)
 
