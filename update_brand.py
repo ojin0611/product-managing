400701c0093a9ef007f4ac7716492b88e6f0576d
Yang Youngjin <ojin0611@gmail.com>
2019-02-13 18:23:14 +0900

    linux windows

--- update_brand.py
+++ update_brand.py
@@ -1,9 +1,15 @@
 import os
 import sys
+import platform
+
+if platform.system() == 'Windows':
+    python_version = ' & python '
+else:
+    python_version = ' ; python3 '
 
 def main():
     brand = sys.argv[1]
-    cmd = "cd crawling & python crawling.py "+brand
+    cmd = "cd crawling"+python_version+ "crawling.py "+brand
 #    cmd = "cd "+brand+" & python "+brand+".py"
     os.system(cmd)
 
