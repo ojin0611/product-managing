7f723fc8f02b56896852883d00550c295ffb9f28
Yang Youngjin <ojin0611@gmail.com>
2019-02-13 17:05:47 +0900

    delete korean in crawling.py

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -14,14 +14,10 @@ def main():
         if brand in value:
             print('crawling method :',key)
             if key=='python':
-                filelist = glob(brand+"/*.py") # 모든 python file 실행할것! 추후에 crawler로 이름 바꿔도 됨.
-#                filelist = glob("/*.py")
-                print(filelist)
-
+                filelist = glob(brand+"/*.py") # file name change recommended
                 for file in filelist:
-                    cmd = "cd "+brand+" & python "+file
-
-                    os.system(cmd) # 이 때 current directory 변경됨
+                    cmd = "cd "+brand+" & python "+brand+".py"
+                    os.system(cmd) # 
                 pass
 
             if key=='js':
