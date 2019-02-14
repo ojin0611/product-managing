8e6f9e90a730a45dfb340e5bec159e1bf1f2fafe
Yang Youngjin <ojin0611@gmail.com>
2019-02-14 17:19:10 +0900

    commit!

---
+++ modules/put_DynamoDB.py
@@ -0,0 +1,3 @@
+
+
+columns = ['name', 'url', 'image', 'color', 'category', 'salePrice', 'originalPrice', 'brand', 'volume', 'info_status', 'discon', 'request_time', 'sale_status', 'confirm_time']
---
+++ modules/upload_s3.py
@@ -0,0 +1,41 @@
+import boto3
+import sys
+import json
+
+# Create an S3 client
+s3 = boto3.client('s3')
+
+# directory + filename 을 넣고 filename으로 저장
+brand = sys.argv[1]
+local_path  = '../data/'+brand+'/compare/'
+s3_path = brand + '/compare/'
+filename = 'new.json'
+bucket_name = 'cosmee-product-data'
+
+# Uploads the given file using a managed uploader, which will split up large
+# files automatically and upload parts in parallel.
+s3.upload_file(local_path + filename, bucket_name, s3_path + filename)
+print('Upload Complete! - ',filename)
+
+
+s3 = boto3.client('s3')
+s3_object = s3.get_object(Bucket=bucket_name, Key=s3_path + filename)
+s3_text = s3_object['Body'].read().decode()
+json_product = json.loads(s3_text)
+for jsonfile in json_product:
+    print(jsonfile.keys())
+    break
+
+'''get S3 bucket list
+# Create an S3 client
+s3 = boto3.client('s3')
+
+# Call S3 to list current buckets
+response = s3.list_buckets()
+
+# Get a list of all bucket names from the response
+buckets = [bucket['Name'] for bucket in response['Buckets']]
+
+# Print out the bucket list
+print("Bucket List: %s" % buckets)
+'''
