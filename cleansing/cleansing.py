a70adf3cf76c39f66c1cc2b0f2e7775f800a72fd
unknown <2sy1202@gmail.com>
2019-02-15 18:52:16 +0900

    module

--- cleansing/cleansing.py
+++ cleansing/cleansing.py
@@ -13,7 +13,7 @@ def main():
     brand = sys.argv[1]
     print('cleansing.py start')
     crawled_data = io_module.get_json("new", brand, "crawling")
-    # clean the crawled_data in the block below  
+    # clean the crawled_data 
     cleansed_data = list(map(cleanseColumns1, crawled_data))
     cleansed_data = list(map(cleanseBrand, cleansed_data))
     cleansed_data = list(filter(None, cleansed_data))   # 취급하지 않는 브랜드의 경우 None값을 return -> filter
@@ -24,7 +24,7 @@ def main():
     cleansed_data = list(map(cleansePrice, cleansed_data))
     cleansed_data = list(map(cleanseColumns2, cleansed_data))
     output = cleansed_data
-    # ----------------------------------------------------
+    # save output as json
     io_module.upload_json(output, brand, "cleansing")
 
 if __name__ == "__main__":
