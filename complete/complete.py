7b57ee68a5d7ce52d1cbe1970defbad367961c4b
dlawjdqls10 <dlawjdqls16@gmail.com>
2019-02-18 11:51:34 +0900

    rough version

--- complete/complete.py
+++ complete/complete.py
@@ -11,69 +11,19 @@ import pprint
 def main():
     brand = sys.argv[1]
     now = datetime.now()
-    request_time = '%s-%s-%s / %s:%s' % (now.year, now.month, now.day, now.hour, now.minute)
+    new_complete = local_module.load_json("new", brand, "compare")
+    old_complete = local_module.load_json("new", brand, "complete")
+    new_skuid_list = []
+    for new_info in new_complete:
+        new_skuid_list.append(new_info['skuid'])
 
-    new_compare = local_module.load_json("new", brand, "cleansing")
-    old_complete = local_module.load_json("new", brand, "compare")
-    sku_attributes = ['brand', 'name', 'color', 'volume', 'type']
+    for old_info in old_complete:
+        if old_info['skuid'] not in new_skuid_list:
+            new_complete.append(old_info)
 
-    #renew_attributes = ['url', 'image', 'salePrice', 'originalPrice']
-
-    # --> old와 new data 비교 => name, color, volume, type 같으면 renew
-    # --> 다르면 old 에 있는지 new에 있는지 확인 : old_only -> discon, new_only -> newpos
-    # 갱신되야 하는 것
-    renew = []
-    before_renew = []
-    # 단종된 것
-    discon = []
-    # 새로 등록 된 것
-    new_pos = []
-
-    for new_dict in new_cleansing:
-        if new_dict not in old_cleansing:
-            for old_dict in old_cleansing:
-                if new_dict['brand'] == old_dict['brand'] and new_dict['name'] == old_dict['name'] and new_dict['color'] == old_dict['color'] and new_dict['volume'] == old_dict['volume'] and new_dict['type'] == old_dict['type']:
-                    new_dict['info_status'] = "갱신요청" # sku 단위는 변화없으나, 부수 항목(price, image, url, sale_status 만 변한 상태
-                    new_dict['discon'] = "#" # 단종이 아님
-                    new_dict['request_time'] = request_time
-                    new_dict['sale_status'] = "#"
-                    new_dict['confirm_time'] = "*"
-                    renew.append(new_dict)
-                    print("갱신요청,단종")
-                    break
-                else:
-                    new_dict['info_status'] = "등록요청"
-                    new_dict['discon'] = "#" # 단종이 아님
-                    new_dict['request_time'] = request_time
-                    new_dict['sale_status'] = "#"
-                    new_dict['confirm_time'] = "*"
-                    new_pos.append(new_dict)
-                    break
-
-    for old_dict in old_cleansing:
-        if old_dict not in new_cleansing:
-            for new_dict in new_cleansing:
-                if new_dict['brand'] == old_dict['brand'] and new_dict['name'] == old_dict['name'] and new_dict['color'] == old_dict['color'] and new_dict['volume'] == old_dict['volume'] and new_dict['type'] == old_dict['type']:
-                    before_renew.append(new_dict) # just for check
-                    pass
-                else:
-                    old_dict['info_status'] = "갱신요청"
-                    old_dict['discon'] = "단종" # 공식 사이트에서 더 이상 그 제품을 찾을 수 없는 경우를 의미.
-                    old_dict['request_time'] = request_time
-                    old_dict['sale_status'] = "#"
-                    old_dict['confirm_time'] = "*"
-                    discon.append(old_dict)
-                    break
-
-    result_json = renew + new_pos + discon
-    # just for check
-    # print(result_json)
-    result_json = sku_naming.sku_naming(result_json)
-#    io_module.upload_json(result_json, brand, "compare")
-    local_module.save_json(result_json, brand, "compare")
-    print("---- compare 및 결과물 저장완료 -----------")
-    local_module.save_json(result_json, brand, "complete")
-
+#    io_module.upload_json(result_json, brand, "complete")
+    local_module.save_json(new_complete, brand, "complete")
+    print("---- complete 및 결과물 저장완료 -----------")
 
 if __name__ == "__main__":
     main()
