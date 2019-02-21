6d3c4f8244121bb5b74b4e6af923dcc2eb3d59eb
Yang Youngjin <ojin0611@gmail.com>
2019-02-21 14:19:57 +0900

    tonymoly ;;

--- crawling/tonymoly/tonymoly.py
+++ crawling/tonymoly/tonymoly.py
@@ -119,11 +119,15 @@ def getItemDetailByUrl(urlList):
 
 print('system :',platform.system())
 if (platform.system() == 'Linux'):
-    from pyvirtualdisplay import Display
-    display = Display(visible=0, size=(800, 800))  
-    display.start()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--disable-extensions')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(chrome_options=options)
+    '''
     driver = webdriver.Chrome('/usr/bin/chromedriver')
-    print('chromedriver open')
+    '''
 else:
     driver = openChromedriver()
 '''
